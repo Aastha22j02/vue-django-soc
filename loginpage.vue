8f20a5ed649ff9feb5490a3eced9bb5397de3158
Aastha Gupta <102406428+Aastha22j02@users.noreply.github.com>
2023-10-28 09:31:44 +0000

    Update loginpage.vue

--- loginpage.vue
+++ loginpage.vue
@@ -1,31 +1,31 @@
 <template>
   <section class="bg-gray-50 dark:bg-gray-900">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            Big Animal
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                    Sign in to your account
-                </h1>
-                <div class="space-y-4 md:space-y-6">
-                    <div>
-                        <label for="username" class="block mb-2 text-sm ml-5 font-medium text-gray-900 dark:text-white">Your username</label>
-                        <input type="username" v-model="username" class="input-field" placeholder="username" required="">
-                    </div>
-                    <div>
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password"  v-model="password" placeholder="••••••••" class="input-field" required="">
-                    </div>
-                    <div class="flex items-center justify-between">                  
-                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                    </div>
-                    <div v-if="error" class="text-red-500 text-center mb-4">{{ error }}</div>
-                    <button type="submit" @click="login" class="button-primary">Login</button>
-                  </div>
+      <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        Big Animal
+      </a>
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Sign in to your account
+          </h1>
+          <div class="space-y-4 md:space-y-6">
+            <div>
+              <label for="username" class="block mb-2 text-sm ml-5 font-medium text-gray-900 dark:text-white">Your username</label>
+              <input type="username" v-model="username" class="input-field" placeholder="username" required="">
             </div>
+            <div>
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+              <input type="password" v-model="password" placeholder="••••••••" class="input-field" required="">
+            </div>
+            <div class="flex items-center justify-between">
+              <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+            </div>
+            <div v-if="error" class="text-red-500 text-center mb-4">{{ error }}</div>
+            <button type="submit" @click="login" class="button-primary">Login</button>
+          </div>
         </div>
+      </div>
     </div>
   </section>
 </template>
@@ -62,6 +62,9 @@ export default {
         })
         .catch((error) => {
           this.error = error.response.data.error;
+          setTimeout(() => {
+            this.error = null; // This will hide the error message after 1 second
+          }, 1000);
         })
     },
   }
@@ -89,5 +92,4 @@ export default {
 .button-primary:hover {
   background-color: #374151;
 }
-
 </style>
